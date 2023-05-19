ffa8383c02a1a7cba9ebb0e5e26b3ea0752762dd
alexis <alexisjesuscordovadelacruz@gmail.com>
2023-05-19 16:08:05 -0600

    Se inicio el rastreo de cambios con git

---
+++ src/main/java/ico/fes/apilol/Apilol2Application.java
@@ -0,0 +1,13 @@
+package ico.fes.apilol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apilol2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apilol2Application.class, args);
+	}
+
+}
