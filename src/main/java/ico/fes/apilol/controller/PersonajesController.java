bc8252706cc77749bcb53ff752eb11b966e6f682
alexis <alexisjesuscordovadelacruz@gmail.com>
2023-05-31 18:39:48 -0600

    Se arreglo el retorno de pagina del get y se crearon los fragmentos para las vistas

--- src/main/java/ico/fes/apilol/controller/PersonajesController.java
+++ src/main/java/ico/fes/apilol/controller/PersonajesController.java
@@ -3,24 +3,24 @@ package ico.fes.apilol.controller;
 import ico.fes.apilol.model.Personaje;
 import ico.fes.apilol.persistencia.PersonajeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
-@RestController
-@RequestMapping("/lol/personaje")
+@Controller
+@RequestMapping("/peticiones")
 public class PersonajesController {
     @Autowired
     private PersonajeRepository personajeRepo;
 
     @GetMapping("/{id}")
-    public Optional<Personaje> getPersonaje(@PathVariable int id){
-        return personajeRepo.findById(id);
+    public String getElement(@PathVariable int id, Model modelo){
+        Personaje personaje = personajeRepo.findById(id).get();
+        modelo.addAttribute("campeon", personaje);
+        return "home";
     }
-
 
 }
 
