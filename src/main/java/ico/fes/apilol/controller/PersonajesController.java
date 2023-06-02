2e4e7a46908f154fc35f26770e5ced5dc10942d8
alexis <alexisjesuscordovadelacruz@gmail.com>
2023-06-02 17:34:41 -0600

    Se terminaron todos los endpoints necesarios, YA NO SE DEBE MODIFICAR ESTE PROYECTO

--- src/main/java/ico/fes/apilol/controller/PersonajesController.java
+++ src/main/java/ico/fes/apilol/controller/PersonajesController.java
@@ -3,25 +3,37 @@ package ico.fes.apilol.controller;
 import ico.fes.apilol.model.Personaje;
 import ico.fes.apilol.persistencia.PersonajeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/peticiones")
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/personajes")
 public class PersonajesController {
     @Autowired
     private PersonajeRepository personajeRepo;
 
     @GetMapping("/{id}")
-    public String getElement(@PathVariable int id, Model modelo){
-        Personaje personaje = personajeRepo.findById(id).get();
-        modelo.addAttribute("campeon", personaje);
-        return "home";
+    public Optional<Personaje> getElement(@PathVariable int id){
+        return personajeRepo.findById(id);
     }
 
+    @GetMapping("/")
+    public Iterable<Personaje> getElements(){
+        return personajeRepo.findAll();
+    }
+
+    // El metodo save puede hacer un insert y un update, si se le manda un id hara un update de existir el id,
+    // en caso contrario (sin id o sin match con un id) hara un insert
+    @PostMapping("/save")
+    public Personaje saveElement(@RequestBody Personaje personaje){
+        return personajeRepo.save(personaje);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteElement(@PathVariable int id){
+        personajeRepo.deleteById(id);
+    }
 }
 
 
